76dd34e3b2eb0bb827809b8efeb4f30a95f3abe2
thinction-a <jakuro94@gmail.com>
2021-01-25 11:32:43 +0900

    21.01.25reStart

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import React, { useState } from 'react';
-import TodoInsert from './components/TodoInsert';
-import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
-import './App.css';
 
 const App = () => {
-
-  const [ todos, setTodos ] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해 보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
-      checked: false,
-    },
-  ])
-
   return (
+    <>
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList todos={todos} />
+        TodoApp을 만들자!
     </TodoTemplate>
+    </>
   );
 }
 
